c34ee0078de92003fe28033d5c18a945a9d4ebd1
Your Name <your_email@example.com>
2026-07-27 10:11:47 +0500

    Added warehouse assets and config

--- app.py
+++ app.py
@@ -1,12 +1,6 @@
-from flask import Flask, render_template, session
-import redis, sqlite3
+from flask import Flask, render_template
 
 app = Flask(__name__)
-app.secret_key = 'super_game_super_secure_key'
-
-cache = redis.StrictRedis(host='localhost', port=6379, db=0)
-conn = sqlite3.connect('supergame.db', check_same_thread=False)
-cur = conn.cursor()
 
 @app.route('/')
 def home():
@@ -14,11 +8,7 @@ def home():
 
 @app.route('/start_battle')
 def start_battle():
-    return "<h1>FX Master Battle Started!</h1><br><a href='/'>Go Back</a>"
-
-@app.route('/upgrade')
-def upgrade():
-    return render_template('upgrade.html')
+    return render_template('battle.html')
 
 if __name__ == '__main__':
     app.run(port=8080)
